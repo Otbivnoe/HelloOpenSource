55627019b5fc751f8e195a76cde34bae9bfe425c
Nikita Ermolenko <gnod94@gmail.com>
2017-03-12 16:22:50 +0600

    Generate targets for each platform

---
+++ HelloOpenSource macOS/HelloOpenSource macOS.h
@@ -0,0 +1,19 @@
+//
+//  HelloOpenSource macOS.h
+//  HelloOpenSource macOS
+//
+//  Created by Nikita Ermolenko on 12/03/2017.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for HelloOpenSource macOS.
+FOUNDATION_EXPORT double HelloOpenSource_macOSVersionNumber;
+
+//! Project version string for HelloOpenSource macOS.
+FOUNDATION_EXPORT const unsigned char HelloOpenSource_macOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HelloOpenSource_macOS/PublicHeader.h>
+
+
---
+++ HelloOpenSource tvOS/HelloOpenSource tvOS.h
@@ -0,0 +1,19 @@
+//
+//  HelloOpenSource tvOS.h
+//  HelloOpenSource tvOS
+//
+//  Created by Nikita Ermolenko on 12/03/2017.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HelloOpenSource tvOS.
+FOUNDATION_EXPORT double HelloOpenSource_tvOSVersionNumber;
+
+//! Project version string for HelloOpenSource tvOS.
+FOUNDATION_EXPORT const unsigned char HelloOpenSource_tvOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HelloOpenSource_tvOS/PublicHeader.h>
+
+
